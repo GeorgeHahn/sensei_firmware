ff4d2a8e17fbee643cebd89ee72471f9be928ed3
Pete Schwamb <pete@schwamb.net>
2017-01-23 13:29:55 -0600

    Return from callback earlier to avoid dropping packets.

--- Listen/Listen.ino
+++ Listen/Listen.ino
@@ -15,21 +15,58 @@
 
 #include "SimbleeCOM.h"
 
+typedef struct packet {
+  unsigned long millis;
+  unsigned int esn;
+  unsigned char data[15];
+  int len;
+  int rssi;
+} packet;
+
+#define BUFFER_SIZE 256
+
+packet packets[BUFFER_SIZE];
+unsigned int head;
+unsigned int tail;
+unsigned int count;
+
 void setup() {
   Serial.begin(115200);
   SimbleeCOM.mode = LOW_LATENCY;
   SimbleeCOM.begin();
+  tail = 0;
+  head = 0;
+  count = 0;
 }
 
 void loop() {
+  if (count > 0) {
+    packet *p = &packets[tail];
+    printf("%08d %d %0x08x ", p->millis, p->rssi, p->esn);
+    
+    for (int i = 0; i < p->len; i++) {
+      printf("%02x ", p->data[i]);
+    } 
+    printf("\r\n");  
+    count--;
+    tail++;
+    if (tail >= BUFFER_SIZE) {
+      tail = 0;
+    }
+  }
 }
 
 void SimbleeCOM_onReceive(unsigned int esn, const char *payload, int len, int rssi)
 {
-  printf("%08d", millis());
-  printf("%d ", rssi);
-  printf("0x%08x ", esn);
-  for (int i = 0; i < len; i++)
-    printf("%02x ", payload[i]);
-  printf("\r\n");
+  packet *p = &packets[head];
+  p->millis = millis();
+  p->esn = esn;
+  memcpy(p->data, payload, len);
+  p->len = len;
+  p-> rssi = rssi;
+  head++;
+  count++;
+  if (head >= BUFFER_SIZE) {
+    head = 0;
+  }
 }
